241d87b1e5afabf892c131b5961a1984017ce768
ss473901 <satoshi.473901.1205@gmail.com>
2021-12-14 19:36:59 +0900

    enemy

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-class Character {
-  constructor(name, hp){
+class Character{
+  constructor(name,hp){
     this.name = name;
     this.hp = hp;
   }
+}
 
-  attack(){
-    console.log(this.name + 'の攻撃!');
+
+class Hero extends Character{
+  constructor(name, hp, exp){
+
+    //継承元のプロパティにアクセス(名前・HPの値をセット)
+    super(name, hp);
+
+    this.exp = exp;
+
+
   }
 }
 
+
+const hero = new Hero('主人公',100,10);
+
+console.log(hero);
